5a1dcbd1626dace63d8ce9d3d22af98c03d96755
Andrey Sorokin <ansorpy@gmail.com>
2023-04-12 23:27:54 +0300

    work

--- logistic/tests.py
+++ logistic/tests.py
@@ -4,6 +4,6 @@ from rest_framework.test import APIClient
 
 class TestSampleView(TestCase):
     def test_view(self):
-        client= APIClient()
+        client = APIClient()
         response = client.get('/api/v1/tests/')
         self.assertEqual(response.data, 'Проверка прошла!!!')
