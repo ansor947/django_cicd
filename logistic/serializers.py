82a154ebbc56169f57a21fb2f665dff83e1e5223
Andrey Sorokin <ansorpy@gmail.com>
2023-04-12 23:39:34 +0300

    ciwork

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -20,19 +20,16 @@ class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = ['id', 'address', 'positions']
-
     def create(self, validated_data):
         positions = validated_data.pop('positions')
         stock = super().create(validated_data)
         for position in positions:
             StockProduct.objects.create(stock=stock, **position)
-
         return stock
 
     def update(self, instance, validated_data):
         positions = validated_data.pop('positions')
         stock = super().update(instance, validated_data)
-        
         for position in positions:
             StockProduct.objects.update_or_create(
                 stock=stock,
